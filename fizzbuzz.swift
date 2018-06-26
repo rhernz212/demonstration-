92abe5e792d445e889446208a3e74a59ad87108f
Ruben Hernandez <ruben.hernandez212@gmail.com>
2018-06-26 15:52:08 -0500

    Create fizzbuzz.swift
    Solution to FizzBuzz problem

---
+++ fizzbuzz.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// FizzBuzz
+
+// create an empty array that can have numbers passed to it
+var arrayOfNumbers = [Int]()
+
+// loop to pass numbers to arrayOfNumbers
+
+for num in 1...100 {
+    arrayOfNumbers.append(num)
+}
+
+// create the loop for the fizzbuzz logic
+
+for num in arrayOfNumbers {
+    if num % 15 == 0 { // checked first to avoid printing only fizz when num % 3 == 0 or num % 5 == 0 condition met
+        print("FizzBuzz")
+    } else if num % 3 == 0 {
+        print("Fizz")
+    } else if num % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(num)
+    }
+}
+
+
